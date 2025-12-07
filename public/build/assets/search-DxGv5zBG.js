abfad637c2b11b1a000e144a55597d5238ca4f06
Joshua Dave Cantorna <143801580+joshuadavecantorna@users.noreply.github.com>
2025-12-07 23:23:16 +0800

    Upgrade to Vite 7 for compatibility with laravel-vite-plugin 2.0

---
+++ public/build/assets/search-DxGv5zBG.js
@@ -0,0 +1,1 @@
+import{c}from"./AppLogoIcon.vue_vue_type_script_setup_true_lang-DLf32hWL.js";const r=c("SearchIcon",[["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]]);export{r as S};
