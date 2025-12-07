ba6911e49b34f110aa348ce1b9e29052b5270117
Joshua Dave Cantorna <143801580+joshuadavecantorna@users.noreply.github.com>
2025-12-07 23:51:11 +0800

    Configure Vite to output manifest.json to correct location

---
+++ public/build/assets/loader-circle-CVFRiqD_.js
@@ -0,0 +1,1 @@
+import{c as e}from"./AppLogoIcon.vue_vue_type_script_setup_true_lang-Br5brvuf.js";const c=e("LoaderCircleIcon",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]);export{c as L};
