57f89480ab9b74fe80fd868ec572b304b111c520
Joshua Dave Cantorna <143801580+joshuadavecantorna@users.noreply.github.com>
2025-12-08 13:11:36 +0800

    Add built assets and manifest for Railway deployment

--- public/build/assets/plus-BwWFmYkd.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./AppLogoIcon.vue_vue_type_script_setup_true_lang-Br5brvuf.js";const a=e("PlusIcon",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]]);export{a as P};
